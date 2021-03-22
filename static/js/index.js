e6880465e3ec641ccc3ef605427d3038185f45a0
VINIT RAJ <vinitraj412@gmail.com>
2021-03-23 01:31:01 +0530

    loginSystemDone

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,5 @@
 // const signUpButton = document.getElementById('signUp');
-var signInButton=document.getElementById('login');
+// var signInButton=document.getElementById('login');
 // console.log(signInButton);
 // // const container = document.getElementById('container');
 
@@ -10,15 +10,15 @@ var passwordTextBox=document.getElementById('password');
 // 	container.classList.add("right-panel-active");
 // });
 
-signInButton.addEventListener('click', () => {
-    // container.classList.remove("right-panel-active");
-    var email=emailTextBox.value;
-    var password=passwordTextBox.value;
-    console.log(email);
-    console.log(password);
-
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "/loginDetails");
-    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhr.send(JSON.stringify({ email: email,password: password}));
-});
+// signInButton.addEventListener('click', () => {
+//     // container.classList.remove("right-panel-active");
+//     var email=emailTextBox.value;
+//     var password=passwordTextBox.value;
+//     console.log(email);
+//     console.log(password);
+//
+//     var xhr = new XMLHttpRequest();
+//     xhr.open("POST", "/loginDetails");
+//     xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+//     xhr.send(JSON.stringify({ email: email,password: password}));
+// });
